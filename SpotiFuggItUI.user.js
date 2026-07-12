d527a9c8747f27e19e6bb0b92038283ef8301e32
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-07-12 03:13:38 +0200

    Update and rename SpotiFuckItUI.user.js to SpotiFuggItUI.user.js

--- SpotiFuggItUI.user.js
+++ SpotiFuggItUI.user.js
@@ -18,8 +18,8 @@
 // @run-at       document-start
 // @homepageURL  https://github.com/Myst1cX/spotifuck-userscript
 // @supportURL   https://github.com/Myst1cX/spotifuck-userscript/issues
-// @updateURL    https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifucklog.user.js
-// @downloadURL  https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifucklog.user.js
+// @updateURL    https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/SpotiFuggItUI.user.js
+// @downloadURL  https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/SpotiFuggItUI.user.js
 // ==/UserScript==
 
 /*
